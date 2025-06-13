cd821a6abdebecd6abd23711cfe81522dcdd1ddf
r2ditto <ryan.ebro@yahoo.com>
2025-06-13 11:22:51 -0400

    install framer motion and implement BlurText component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import { Container } from "@mantine/core";
-import Table from "../components/Table";
+
+import Table from "@/components/Table";
+import BlurText from "@/components/BlurText";
 
 export default function Home() {
   return (
     <main>
-      <Container fluid style={{ paddingTop: 100, paddingBottom: 100 }}>
+      <Container fluid style={{ paddingTop: 50, paddingBottom: 100 }}>
+        <BlurText
+          text="Stable. Reliable. Table. 🫠"
+          delay={150}
+          animateBy="words"
+          direction="top"
+        />
+
         <Table />
       </Container>
     </main>
